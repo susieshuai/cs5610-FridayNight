4a19c9af3cf7872d7680f9b8ecd37c7e2b7e7a50
shuais <shuai.s@northeastern.edu>
2022-03-31 21:11:38 -0700

    set up server

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,11 @@
 const express = require('express')
-const db = require('./db')
+
 const app = express()
 
-
-
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended:false}))
-
-app.use(express.json())
-const cors = require('cors')
-require('dotenv').config()
-
-const port = process.env.PORT ||5000
-app.use(cors())
-
-app.use(express.static('public'))
-app.set('view engine', 'pug')
-app.set('views', './views')
-
-const product = require('./routes/product.js')
-app.use('/products', product.router)
-
-app.get('/', function (req, res) {
-    res.send('hello world')
+app.get('/', (req, res) => {
+    res.send('Hello')
 })
 
-
-db.dbConnect().then(() => app.listen(port, () => {
-    console.log('app is running on port 5000')
-}))
+app.listen(5000, () => {
+    console.log('server is running on port 5000');
+})
