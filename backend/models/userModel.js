b9c01f73c614dd9a3dae55fb0cdf6ae7885b7108
shuais <shuai.s@northeastern.edu>
2022-04-01 13:21:25 -0700

    create user model and product model

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const userModel = mongoose.model('users', userSchema)
+
+module.exports = userModel
