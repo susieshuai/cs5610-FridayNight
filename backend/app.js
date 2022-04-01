9b63ce91eea62c5159ef840b7872a2a24b87c0b5
shuais <shuai.s@northeastern.edu>
2022-03-31 21:31:13 -0700

    add routes to get all products/single product from backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const products = require('./data/products')
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello')
+})
+
+app.get('/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id
+    const product = products.find(product => {
+        return product._id === id
+    })
+    res.json(product)
 })
 
 app.listen(5000, () => {
