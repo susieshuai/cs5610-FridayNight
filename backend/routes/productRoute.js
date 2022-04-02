bc9681dec63ccdd029eb1d7b5d187cd711516151
shuais <shuai.s@northeastern.edu>
2022-04-01 17:40:51 -0700

    add product controller, build basis of CRUD operations

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,30 +1,11 @@
 const express = require('express')
+const productController = require('../controllers/productController')
 const productModel = require('../models/productModel')
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    try {
-        const products = await productModel.find({})
-        res.json(products)
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.get('/', productController.getAllProducts)
 
-router.get('/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        const product = await productModel.findById(id)
-        if (!product) {
-            console.log('no item found with this id');
-        }
-        else {
-            res.json(product)
-        }
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.get('/:id', productController.getOneProduct)
 
 module.exports = router
