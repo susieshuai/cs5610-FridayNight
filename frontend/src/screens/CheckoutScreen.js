01474fef066fa6fad603fdd9002060c4d3331b25
shuais <shuai.s@northeastern.edu>
2022-04-07 21:21:56 -0700

    add frontend checkout screen

---
+++ frontend/src/screens/CheckoutScreen.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Image, Button, Table, } from 'react-bootstrap'
+import Message from '../components/Message'
+
+const CheckoutScreen = () => {
+    const cart = useSelector(state => state.cart)
+
+    const confirmOrder = () => {
+        console.log('confirm order');
+    }
+
+    return (
+        <>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h2>My Items</h2>
+                    {cart.cartItems.length === 0 ? (
+                        <Message>Cart is empty</Message>
+                    ) : (
+                        //  cart items list
+                        <ListGroup variant='flush'>
+                            {cart.cartItems.map((item, index) => (
+                                <ListGroup.Item key={index}>
+                                    <Row>
+                                        <Col md={1}>
+                                            <Image
+                                                src={item.cover}
+                                                alt={item.name}
+                                                fluid
+                                                rounded
+                                            />
+                                        </Col>
+                                        <Col>
+                                            <Link to={`/details/${item.product}`}>
+                                                {item.name}
+                                            </Link>
+                                        </Col>
+                                        <Col md={4}>
+                                            $ {item.price} X {item.qty}   = $ {item.qty * item.price}
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    )}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <h2>Checkout</h2>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Table striped bordered hover>
+                                <thead>
+                                </thead>
+                                <tbody>
+                                    <tr style={{ color: 'red', fontSize: '15px' }}>
+                                        <td>Subtotal
+                                            ({cart.cartItems.reduce((acc, item) => acc + item.qty, 0)} items)</td>
+                                        <td> ${cart.cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}</td>
+                                    </tr>
+                                    <tr style={{ fontSize: '15px' }}>
+                                        <td >Delivery</td>
+                                        <td style={{ color: 'red' }}> Free</td>
+                                    </tr>
+                                    <tr style={{ fontSize: '15px' }}>
+                                        <td>Estimated tax rate</td>
+                                        <td> 12%</td>
+                                    </tr>
+                                    <tr>
+                                        <td><b>Total</b></td>
+                                        <td><b>${cart.cartItems.reduce((acc, item) => (acc + item.qty * item.price) * 1.12, 0).toFixed(2)}</b></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button
+                                type='button'
+                                className='btn-block'
+                                disabled={cart.cartItems.length === 0}
+                                style={{
+                                    width: '350px',
+                                    display: 'block',
+                                    margin: 'auto',
+                                }}
+                                onClick={confirmOrder}
+                            >
+                                Confirm
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </ListGroup.Item>
+            </ListGroup>
+        </>
+    )
+}
+
+export default CheckoutScreen
