2f7667a43c0aabb7ede4a9f349023f4202fb95b8
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-04-07 18:44:55 -0700

    complete function register

--- frontend/src/screens/DetailScreen.js
+++ frontend/src/screens/DetailScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Route, Routes, useParams } from 'react-router-dom'
+import {  useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { listProductDetails } from '../actions/productActions'
