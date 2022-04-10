843cc7cf4773bdf1540c9dceb9175d1175a72dbd
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-04-09 21:18:29 -0700

    Update AdScreen.js

--- frontend/src/screens/AdScreen.js
+++ frontend/src/screens/AdScreen.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router-dom'
 const AdScreen = () => {
   return (
     <>
-    <Link className='btn btn-secondary my-5' to='/'>
-        Back to Home
-      </Link>
     <Ad/>
     </>
   )
