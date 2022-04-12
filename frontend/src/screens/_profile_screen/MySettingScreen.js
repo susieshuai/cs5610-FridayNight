be5f6386e3801a7d61d3b59c51014bfcb794db17
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-04-12 04:16:42 -0700

    modify profile page

--- frontend/src/screens/_profile_screen/MySettingScreen.js
+++ frontend/src/screens/_profile_screen/MySettingScreen.js
@@ -7,7 +7,7 @@ import { getUserDetails, updateUserDetails } from "../../actions/userActions";
 import { useNavigate } from 'react-router-dom';
 
 
-const MySettingScreen = ({ userInfo, user }) => {
+const MySettingScreen = () => {
 
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
@@ -17,6 +17,12 @@ const MySettingScreen = ({ userInfo, user }) => {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const userDetails = useSelector((state) => state.userDetails)
+  const { user } = userDetails
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
   const { success } = userUpdateProfile
