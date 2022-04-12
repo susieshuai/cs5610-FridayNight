460d7da1e3c39641cf4c8c3b86e757d89cb292c0
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-04-12 01:00:29 -0700

    modify details of setting screen

--- frontend/src/screens/_profile_screen/MySettingScreen.js
+++ frontend/src/screens/_profile_screen/MySettingScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import Loader from '../../components/Loader'
 import Message from '../../components/Message'
 import { getUserDetails, updateUserDetails } from "../../actions/userActions";
 
