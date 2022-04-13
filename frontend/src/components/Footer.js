4073a99bf9afb8618caba314a0897bd1c1b8e6ab
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-04-13 15:16:23 -0700

    submit for hmpage

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3 mt-4' style={{ color: 'grey', fontSize: '12px' }}>&copy;  2022 FridayNight Brands, Inc.</Col>
+          <Col className='text-center py-3 mt-4' style={{ fontSize: '14px' }}>&copy;  2022 FridayNight Brands, Inc.</Col>
         </Row>
         <Col className='text-center' style={{ fontSize: '25px' }}><i className="fa-solid fa-dragon"></i></Col>
         <Col className='text-center'>Expect More, Pay Less.</Col>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,13 +6,13 @@ import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <>
-      <Card className='my-3 py-3 rounded' >
+      <Card className='my-3 py-3 rounded bg-dark' >
         <Link to={`/details/${product._id}`}>
-          <Card.Img src={product.cover} variant='top' />
+          <Card.Img src={product.cover} variant='top' alt={product.name}/>
         </Link>
         <Card.Body>
           <Link to={`/details/${product._id}`}>
-            <Card.Title><small>{product.name}</small></Card.Title>
+            <Card.Title>{product.name}</Card.Title>
           </Link>
           <Card.Text as='div'>
             <Rating value={product.rating} text={` ${product.numReviews} views`}/>
